1e91f5a3b99c17f9761663aa13330844dbb14a6f
skpal123 <somanskpal77@gmail.com>
2019-01-23 19:57:52 +0600

    message

--- server/ERPWebApiService/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERPWebApiService.WebApiApplication" Language="C#" %>
--- server/ViewModel/Model/PermissionView.cs
+++ server/ViewModel/Model/PermissionView.cs
@@ -9,29 +9,28 @@ namespace ViewModel.Model
     public class PermissionView
     {
         public Guid Id { get; set; }
-
+        public Guid MenuId { get; set; }
+        public Guid SubmenuId { get; set; }
+        public Guid ModuleId { get; set; }
         public Guid? RoleId { get; set; }
 
-        public Guid? ModuleId { get; set; }
+        public int ModuleSeqId { get; set; }
 
         public string ModuleName { get; set; }
-
-        public Guid? MenuId { get; set; }
-
         public string MenuName { get; set; }
-
-        public Guid? SubMenuId { get; set; }
-
         public string SubMenuName { get; set; }
+        public int MenuSeqId { get; set; }
+
+        public int SubmenuSeqId { get; set; }
 
         public Guid? ItemId { get; set; }
 
         public string ItemName { get; set; }
 
-        public Guid? PermissionId { get; set; }
+        public Guid PermissionId { get; set; }
 
         public string PermissionName { get; set; }
 
-        public bool Permission { get; set; }
+        public bool IsPermission { get; set; }
     }
 }
